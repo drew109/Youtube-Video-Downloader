105d3a70dfee0bdcceccb501b034150b16ac1676
ykyk21222 <andrewrules903@gmail.com>
2022-03-14 20:49:15 -0400

    test

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from pytube import *
 import os
 
 
-#make break in else 
+#make break in else
 while True:
     #type the url
     inputted_url = input('Enter the video: ')
@@ -11,14 +11,14 @@ while True:
     #Get everything after the = in the url
     head, sep, tail = inputted_url.partition('=')
 
-    #put together the url after being seperated 
+    #put together the url after being seperated
     if inputted_url == 'https://www.youtube.com/watch?v='+ tail:
         the_video = YouTube(url)
         print("--------------------Video Title--------------------")
 
         #prints the video title
         print(the_video.title)
-        
+
         #gives the user the best resoultion usally 1080p
         the_video = the_video.streams.get_highest_resolution()
 
@@ -30,13 +30,13 @@ while True:
 print("--------------------Downloading--------------------")
 
 while True:
-    # Made the decsion to be able to make a mp4 file 
+    # Made the decsion to be able to make a mp4 file
     inputted_decision  = input('Mp4 or Mp3: ')
     if inputted_decision == 'Mp4' or inputted_decision == 'mp4' or inputted_decision == '4':
         original_file = the_video.download()
         break
 
-    # Made the decsion to be able to make a mp3 file 
+    # Made the decsion to be able to make a mp3 file
     elif inputted_decision == 'Mp3' or inputted_decision == 'mp3' or inputted_decision == '3':
         original_file = the_video.download()
         base, ext = os.path.splitext(original_file)
@@ -53,3 +53,4 @@ print("--------------------Downloaded--------------------")
 
 
 #For the understanding of the checker https://bit.ly/3tSpumR that helped me to understand
+#Thank you for the help with file management with .gitignore (
